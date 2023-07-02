c46932efdf696ac5244118569c5325cb9dda9fb9
catnparrot <idiotpi3rrot@gmail.com>
2023-07-03 00:28:20 +0900

    쿼리문 추가(레코드 양산)

--- spring실습쿼리문.sql
+++ spring실습쿼리문.sql
@@ -15,4 +15,7 @@ PRIMARY KEY (bno);
 INSERT INTO tbl_board (bno, title, content, writer)
 VALUES (seq_board.nextval, '테스트 제목', '테스트내용', 'user00');
 
+INSERT INTO tbl_board (bno, title, content, writer)
+(SELECT seq_board.nextval, title, content, writer FROM tbl_board);
+
 COMMIT;
