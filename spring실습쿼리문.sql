eb04b76a81c0608f5812e258194bfa3984ac6616
DESKTOP-6FLLH9U\503 <idiotpi3rrot@gmail.com>
2023-07-04 08:40:58 +0900

    쿼리문 업데이트(tbl_reply 추가)

--- spring실습쿼리문.sql
+++ spring실습쿼리문.sql
@@ -18,4 +18,25 @@ VALUES (seq_board.nextval, '테스트 제목', '테스트내용', 'user00');
 INSERT INTO tbl_board (bno, title, content, writer)
 (SELECT seq_board.nextval, title, content, writer FROM tbl_board);
 
+
+
+
+CREATE TABLE tbl_reply(
+	rno number(10, 0),
+	bno number(10, 0) NOT NULL,
+	reply varchar2(1000) NOT NULL,
+	replyer varchar2(50) NOT NULL,
+	replyDate DATE DEFAULT sysdate,
+	updateDate DATE DEFAULT sysdate
+);
+
+CREATE SEQUENCE seq_reply;
+
+ALTER TABLE tbl_reply ADD CONSTRAINT pk_reply PRIMARY KEY (rno);
+
+ALTER TABLE tbl_reply ADD CONSTRAINT fk_reply_board
+FOREIGN KEY (bno) REFERENCES tbl_board(bno);
+
+
+
 COMMIT;
